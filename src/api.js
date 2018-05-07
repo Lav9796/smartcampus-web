8babf3b6b63d872e1bf92fe8dabe903711d40046
ShavaShav <zachshaver@gmail.com>
2018-05-06 23:34:05 -0400

    Break apiFetch into functions

--- src/api.js
+++ src/api.js
@@ -2,6 +2,8 @@ const jwtDecode = require('jwt-decode');
 
 const API_ROOT = `${process.env.REACT_APP_API_URL}/api`;
 
+var currentlyRefreshing = false;
+
 /**
  * Calls SmartCampus API with proper options and formats response
  * @param  {String}  path     Endpoint to hit (e.g. '/events')
@@ -10,76 +12,91 @@ const API_ROOT = `${process.env.REACT_APP_API_URL}/api`;
  * @return {Promise(Object)}  Response containing 'status' and 'body'
  */
 const apiFetch = (path, method = 'GET', body = null) => {
-
-  return tryRefreshToken().then(() => {
-    const options = {
-      method,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    };
-
-    // Add POST/PUT body, if given
-    if (body) {
-      options.body = JSON.stringify(body);
+  return new Promise(function(resolve, reject){
+    if (isTokenExpired()) {
+      console.log("token expired");
+      // For multiple async requests, the first will 
+      // refresh the token and the others will wait.
+      var _refreshCheck = setInterval(function() {
+        if (!currentlyRefreshing) {
+          console.log(path + " done waiting.");
+          clearInterval(_refreshCheck);
+          return refreshToken().then(() => {
+            console.log(path + " calling api..");
+            return apiWrapper(path, method, body);
+          });
+        } else {
+          console.log(path + " waiting for refreshed token..");
+        }
+      }, 50);
+    } else {
+      console.log("token not expired, calling api");
+      return apiWrapper(path, method, body);
     }
-
-    // Add token as header, if in storage
-    const token = localStorage.getItem('token');
-    if (token) {
-      options.headers['Authorization'] = `Bearer ${token}`;
-    }
-
-    // Call the API
-    return fetch(`${API_ROOT}${path}`, options).then(response => {
-      return response.json().then(json => {
-        
-        // Wrap response json body with status code
-        const status_json = {
-          status: response.status,
-          body: json
-        };
-
-        // Reject promise if bad response
-        return response.ok ? status_json : Promise.reject(status_json);
-      });
-    });
   });
 };
 
-// Attempts to refresh the stored JWT token if expired
-function tryRefreshToken() {
-  return new Promise(function(resolve, reject){
-
-    const token = localStorage.getItem('token');
-    if (!token) {
-      resolve(); // no token to refresh
-    } else {
-      const jwt = jwtDecode(token);
-      var current_time = Date.now() / 1000;
-
-      if ( current_time < jwt.exp ) {
-        resolve(); // token still good
-      } else {
-        const options = {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${token}`
-          }
-        };
-
-        return fetch(`${API_ROOT}/user/refresh`, options).then(response => {
-          return response.json().then(json => {
-            if (response.ok) {
-              localStorage.setItem('token', json.user.token);
-            }
-            resolve(); // regardless if refresh worked
-          });
-        });
-      }
+function apiWrapper (path, method = 'GET', body = null) {
+  const options = {
+    method,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     }
+  };
+
+  // Add POST/PUT body, if given
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+
+  // Add token as header, if in storage
+  const token = localStorage.getItem('token');
+  if (token) {
+    options.headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  // Call the API
+  return fetch(`${API_ROOT}${path}`, options).then(response => {
+    return response.json().then(json => {
+      
+      // Wrap response json body with status code
+      const status_json = {
+        status: response.status,
+        body: json
+      };
+
+      // Reject promise if bad response
+      return response.ok ? status_json : Promise.reject(status_json);
+    });
+  });
+}
+
+/**
+ * Checks if token is expired.
+ * @return {Boolean} true if expired, false otherwise.
+ */
+function isTokenExpired() {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return false; // no token
+  } else {
+    const jwt = jwtDecode(token);
+    const current_time = Date.now() / 1000;
+    return current_time >= jwt.exp;
+  }
+}
+
+// Attempts to refresh the stored JWT token if expired
+function refreshToken() {
+  currentlyRefreshing = true;
+
+  return apiWrapper('/user/refresh', 'POST').then(response => {
+    localStorage.setItem('token', response.body.user.token);
+    currentlyRefreshing = false;
+  }).catch(error => {
+    currentlyRefreshing = false;
   });
 };
 
